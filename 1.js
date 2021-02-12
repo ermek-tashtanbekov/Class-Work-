fe28dc1c5fe8c22c557b9ba395a05089efee8e0e
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-12 22:56:57 +0600

    thri class- works

--- 1.js
+++ 1.js
@@ -14,22 +14,54 @@ document.body.addEventListener('click', function(e){
 });
 */
 /////////////////////
-document.body.addEventListener('mousedown', function(e){
-    let divv = document.createElement('div');
-    divv.style.width = '30px'; 
+ //document.body.addEventListener('mousedown', function(e){
+   //  let divv = document.createElement('div');
+//     divv.style.width = '30px'; 
+//     divv.style.height = '30px'; 
+//     divv.style.position ="absolute";
+//     divv.style.borderRadius = '50%'
+//     divv.style.top = e.clientY - 15 + 'px';
+//     divv.style.left = e.clientX - 15 + 'px';
+//     document.body.append(divv); 
+//     if(e.which == 1){
+//        divv.style.backgroundColor = 'red';
+//     }
+//     if(e.which == 3){
+//         divv.style.backgroundColor = 'blue';
+//         }
+//     document.body.addEventListener('contextmenu', function(e){
+//         e.preventDefault()
+//     });
+
+
+ //});
+
+
+ document.addEventListener('mousedown', function(e){
+     let divv = document.createElement('div');
+     divv.style.width = '30px'; 
     divv.style.height = '30px'; 
     divv.style.position ="absolute";
     divv.style.borderRadius = '50%'
     divv.style.top = e.clientY - 15 + 'px';
     divv.style.left = e.clientX - 15 + 'px';
     document.body.append(divv); 
-    if(e.which == 1){
-       divv.style.backgroundColor = 'red';
-    }
-    if(e.which == 3){
-        divv.style.backgroundColor = 'blue';
+ 
+
+        if(e.ctrlKey){
+            divv.style.borderRadius = "0px"; 
+            divv.style.backgroundColor = 'red';
         }
-    document.body.addEventListener('contextmenu', function(e){
-        e.preventDefault()
-    })
-});
+        if(e.shiftKey){
+            divv.style.borderRadius = "0px";
+            divv.style.width = "100px"
+            divv.style.backgroundColor = 'blue';
+        }
+        if(e.altKey){
+            divv.style.borderRadius = "0px";
+            divv.style.width = "100px";
+            divv.style.heigh = "100px";
+          }
+
+
+        });
