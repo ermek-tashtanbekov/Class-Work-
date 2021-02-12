7f5181c974c08c1aa03f1626d16dd6b1ad799bcd
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-12 22:31:23 +0600

    second class- work

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-
+/*
 document.body.addEventListener('click', function(e){
     let divv = document.createElement('div');
     divv.style.width = '30px'; 
@@ -11,4 +11,33 @@ document.body.addEventListener('click', function(e){
           document.body.append(divv);
 
 
+});
+*/
+/////////////////////
+document.body.addEventListener('mousedown', function(e){
+    let divv = document.createElement('div');
+    if(e.which == 1){
+        divv.style.width = '30px'; 
+          divv.style.height = '30px'; 
+          divv.style.position ="absolute";
+          divv.style.borderRadius = '50%'
+          divv.style.backgroundColor = 'red';
+          divv.style.top = e.clientY - 15 + 'px';
+          divv.style.left = e.clientX - 15 + 'px';
+          document.body.append(divv); 
+    }
+    if(e.which == 3){
+        divv.style.width = '30px'; 
+        divv.style.height = '30px'; 
+        divv.style.position ="absolute";
+        divv.style.borderRadius = '50%'
+        divv.style.backgroundColor = 'blue';
+        divv.style.top = e.clientY - 15 + 'px';
+        divv.style.left = e.clientX - 15 + 'px';
+        document.body.append(divv);
+        console.log(e) 
+    }
+    document.body.addEventListener('contextmenu', function(e){
+        e.preventDefault()
+    })
 });
